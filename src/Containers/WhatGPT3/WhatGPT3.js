60eee48c30419fa5ab4b26e1c523234edea65a5a
Qasim <mq12113@gmail.com>
2024-02-06 14:52:03 +0000

    started hat GPT3 section

--- src/Containers/WhatGPT3/WhatGPT3.js
+++ src/Containers/WhatGPT3/WhatGPT3.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import './WhatGPT3.js'
+import './WhatGPT3.css'
+import Feature from '../Feature/Feature.js'
 
 const  WhatGPT3 = () => {
   return (
-    <div>
-      What GPT3
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+    <div className='gpt3__whatgpt3-feature'>
+    <Feature />
+    </div>
+    <div className="gpt3__whatgpt3-heading">
+    <h1 className='gradient__text'>
+    The possibilities are beyond your imagination
+    </h1>
+    <p>Explore The Library</p>
+
+    </div>
+    <div className="gpt3__whatgpt3-container">
+    <Feature />
+    <Feature />
+    <Feature />
+    </div>
     </div>
   )
 }
